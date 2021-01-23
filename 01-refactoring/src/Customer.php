299cdb89e7f9c194f8db5464a30368651b776a31
rocla <claudio.polo@gmail.com>
2021-01-23 16:25:26 -0300

    08:movi metodo obtainCharge a la clase Rental

--- 01-refactoring/src/Customer.php
+++ 01-refactoring/src/Customer.php
@@ -1,77 +1,63 @@
 <?php
 
 namespace Refactoring;
-class Customer {
-    private  $_name;
+class Customer
+{
+    private $_name;
     private $_rentals = array();
 
-    function __construct($name){
+    public function __construct($name)
+    {
         $this->_name = $name;
     }
 
-    public function addRental(Rental $arg) {
+    public function addRental(Rental $arg)
+    {
         $this->_rentals[] = $arg;
     }
 
-    public function getName (){
+    public function getName()
+    {
         return $this->_name;
     }
 
-public function statement() {
-    $totalAmount = 0;
-    $frequentRenterPoints = 0;
-    $rentals = $this->_rentals;
+    public function statement()
+    {
+        $totalAmount = 0;
+        $frequentRenterPoints = 0;
+        $rentals = $this->_rentals;
 
-    $result = "Rental Record for " . $this->getName() . "\n";
+        $result = "Rental Record for " . $this->getName() . "\n";
 
-    foreach($rentals as $rental) {
-$thisAmount=$this->obtainCharge($rental);
+        foreach ($rentals as $rental) {
+                      $thisAmount=$rental->obtainCharge($rental);
 
   
 
-    $totalAmount += $thisAmount;
+            $totalAmount += $thisAmount;
 
-        // add frequent renter points
-    $frequentRenterPoints++;
+            // add frequent renter points
+            $frequentRenterPoints++;
 
-    // add bonus for a two day new release rental
-    if (($rental->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
+            // add bonus for a two day new release rental
+            if (($rental->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
     &&
-    $rental->getDaysRented() > 1)
-        $frequentRenterPoints++;
+    $rental->getDaysRented() > 1) {
+                $frequentRenterPoints++;
+            }
 
 
-    //show figures for this rental
-    $result .= "\t" . $rental->getMovie()->getTitle() .  "\t" . $thisAmount . "\n";
+            //show figures for this rental
+            $result .= "\t" . $rental->getMovie()->getTitle() .  "\t" . $thisAmount . "\n";
+        }
+        //add footer lines
+        $result .=  "Amount owed is " . $totalAmount . "\n";
+        $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
 
+
+        return $result;
     }
-    //add footer lines
-    $result .=  "Amount owed is " . $totalAmount . "\n";
-    $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
 
 
-    return $result;
+
 }
-
-
-    private function obtainCharge($rental) 
-    {
-    $thisAmount = 0;
-        switch ($rental->getMovie()->getPriceCode())
-    {
-    case Movie::REGULAR:
-        $thisAmount += 2;
-        if ($rental->getDaysRented() > 2)
-            $thisAmount += ($rental->getDaysRented() - 2) * 1.5;
-        break;
-    case Movie::NEW_RELEASE:
-        $thisAmount += $rental->getDaysRented() * 3;
-        break;
-    case Movie::CHILDRENS:
-        $thisAmount += 1.5;
-        if ($rental->getDaysRented() > 3)
-            $thisAmount += ($rental->getDaysRented() - 3) * 1.5;
-        break;
-    }
-    return $thisAmount;
-    }
--- 01-refactoring/src/Rental.php
+++ 01-refactoring/src/Rental.php
@@ -16,4 +16,26 @@ public function getDaysRented() {
 public function getMovie() {
     return $this->_movie;
 }
+ public function obtainCharge($rental)
+    {
+        $thisAmount = 0;
+        switch ($rental->getMovie()->getPriceCode()) {
+    case Movie::REGULAR:
+        $thisAmount += 2;
+        if ($rental->getDaysRented() > 2) {
+            $thisAmount += ($rental->getDaysRented() - 2) * 1.5;
+        }
+        break;
+    case Movie::NEW_RELEASE:
+        $thisAmount += $rental->getDaysRented() * 3;
+        break;
+    case Movie::CHILDRENS:
+        $thisAmount += 1.5;
+        if ($rental->getDaysRented() > 3) {
+            $thisAmount += ($rental->getDaysRented() - 3) * 1.5;
+        }
+        break;
+    }
+        return $thisAmount;
+    }
 }
